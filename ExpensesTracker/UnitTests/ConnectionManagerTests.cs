1ae967b5a64fe7dcca0dc5440fd571489049781d
Stephanie Fisca <stephaniefisca02@gmail.com>
2024-04-11 12:54:04 +0300

    Rename UT files

--- ExpensesTracker/UnitTests/ConnectionManagerTests.cs
+++ ExpensesTracker/UnitTests/ConnectionManagerTests.cs
@@ -10,35 +10,6 @@ using NUnit.Framework.Legacy;
 namespace ExpensesTracker.Tests
 {
     [TestFixture]
-    public class SplashTests
-    {
-        [Test]
-        public void Timer_Tick_Event_Starts_And_Increments_ProgressBar()
-        {
-            var userRepositoryMock = new Mock<IUserRepository>();
-            var splashForm = new Splash(userRepositoryMock.Object);
-            ProgressBar progressBar = splashForm.MyProgressBar;
-            int expectedValue = 1;
-
-            splashForm.Timer1_Tick(null, EventArgs.Empty);
-
-            Assert.That(expectedValue, Is.EqualTo(progressBar.Value)); ;
-        }
-
-        [Test]
-        public void Timer_Tick_Event_Completes_ProgressBar_And_Shows_Login_Form()
-        {
-            var userRepositoryMock = new Mock<IUserRepository>();
-            var splashForm = new Splash(userRepositoryMock.Object);
-
-            splashForm.Timer1_Tick(null, EventArgs.Empty);
-
-            ClassicAssert.IsFalse(splashForm.Visible);
-
-        }
-
-    }
-    [TestFixture]
     public class SqlConnectionManagerTests
     {
         [Test]
---
+++ ExpensesTracker/UnitTests/SplashTests.cs
@@ -0,0 +1,42 @@
+﻿using ExpensesTracker.RepositoryPattern.Interfaces;
+using Moq;
+using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.UnitTests
+{
+    [TestFixture]
+    public class SplashTests
+    {
+        [Test]
+        public void Timer_Tick_Event_Starts_And_Increments_ProgressBar()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var splashForm = new Splash(userRepositoryMock.Object);
+            ProgressBar progressBar = splashForm.MyProgressBar;
+            int expectedValue = 1;
+
+            splashForm.Timer1_Tick(null, EventArgs.Empty);
+
+            Assert.That(expectedValue, Is.EqualTo(progressBar.Value)); ;
+        }
+
+        [Test]
+        public void Timer_Tick_Event_Completes_ProgressBar_And_Shows_Login_Form()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var splashForm = new Splash(userRepositoryMock.Object);
+
+            splashForm.Timer1_Tick(null, EventArgs.Empty);
+
+            ClassicAssert.IsFalse(splashForm.Visible);
+
+        }
+
+    }
+}
